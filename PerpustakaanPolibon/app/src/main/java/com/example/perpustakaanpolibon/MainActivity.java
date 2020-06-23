c0b3a7d49d32b02005b3cb787eb890921f0807a2
wina557 <azizahwinasriwinarsih24@gmail.com>
2020-06-23 11:16:22 +0700

    button login&tertib

--- PerpustakaanPolibon/app/src/main/java/com/example/perpustakaanpolibon/MainActivity.java
+++ PerpustakaanPolibon/app/src/main/java/com/example/perpustakaanpolibon/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.perpustakaanpolibon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.btntertib);
+
+        Button btn2 = (Button) findViewById(R.id.btnlogin);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent inte = new Intent(MainActivity.this, Tertib.class);
+                startActivity(inte);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent inte = new Intent(MainActivity.this, Login.class);
+                startActivity(inte);
+            }
+        });
     }
 }
---
+++ PerpustakaanPolibon/app/src/main/java/com/example/perpustakaanpolibon/Tertib.java
@@ -0,0 +1,14 @@
+package com.example.perpustakaanpolibon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tertib extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tertib);
+    }
+}
