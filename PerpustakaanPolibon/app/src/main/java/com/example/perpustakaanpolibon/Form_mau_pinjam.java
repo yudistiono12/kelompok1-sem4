eb187d73b9fdc817c6f5d3dbc70f2c6c90e4c597
Rizky Akbar <rizkyakbar2000@gmail.com>
2020-06-29 09:16:30 +0700

    form mau login dengan keterangan sementara

---
+++ PerpustakaanPolibon/app/src/main/java/com/example/perpustakaanpolibon/Form_mau_pinjam.java
@@ -0,0 +1,14 @@
+package com.example.perpustakaanpolibon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Form_mau_pinjam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_mau_pinjam);
+    }
+}
