8945b4bf27b9bed4bfe1a264da60e94840e9d513
Haroen <elmahdi869@yahoo.co.id>
2020-06-30 15:43:53 +0700

    perbaikan registrasi + lanjut API code
    - memperbaiki form register
    - melanjutkan code api

--- PerpustakaanBeta/app/src/main/java/com/polije/perpustakaanbeta/SignUp.java
+++ PerpustakaanBeta/app/src/main/java/com/polije/perpustakaanbeta/SignUp.java
@@ -2,13 +2,90 @@ package com.polije.perpustakaanbeta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.polije.perpustakaanbeta.api.RegisterAPI;
+import com.polije.perpustakaanbeta.model.Value;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class SignUp extends AppCompatActivity {
+    public static final String URL = "";
+    private RadioButton radioJKButton;
+    private ProgressDialog progress;
+    String nim,nama,fakultas,prodi;
+
+    @BindView(R.id.radiojk) RadioGroup radioGroup;
+    @BindView(R.id.nim)  EditText editTextnim;
+    @BindView(R.id.nama) EditText editTextnama;
+    @BindView(R.id.fakultas) EditText editTextfakultas;
+    @BindView(R.id.prodi) EditText editTextprodi;
+
+
+    @OnClick(R.id.buttonregis)void daftar() {
+        //menampilkan progress dialog
+        progress = new ProgressDialog(this);
+        progress.setCancelable(false);
+        progress.setMessage("Loading...");
+        progress.show();
+
+        nim = editTextnim.getText().toString();
+        fakultas = editTextfakultas.getText().toString();
+        prodi = editTextprodi.getText().toString();
+
+        int selectedId = radioGroup.getCheckedRadioButtonId();
+        radioJKButton = (RadioButton) findViewById(selectedId);
+        String jk = radioJKButton.getText().toString();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        RegisterAPI api = retrofit.create(RegisterAPI.class);
+        Call<Value> call = api.daftar(nim,nama,fakultas,prodi,jk);
+
+        call.enqueue(new Callback<Value>() {
+            @Override
+            public void onResponse(Call<Value> call, Response<Value> response) {
+                String value = response.body().getValue();
+                String message = response.body().getMessage();
+                progress.dismiss();
+
+                if(value.equals("1")) {
+                    Toast.makeText(SignUp.this, message, Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(SignUp.this, message, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Value> call, Throwable t) {
+                t.printStackTrace();
+                progress.dismiss();
+                Toast.makeText(SignUp.this,"Jaringan Error!",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
     }
 }
--- PerpustakaanBeta/app/src/main/java/com/polije/perpustakaanbeta/model/Value.java
+++ PerpustakaanBeta/app/src/main/java/com/polije/perpustakaanbeta/model/Value.java
@@ -1,19 +1,24 @@
 package com.polije.perpustakaanbeta.model;
 
+import java.util.List;
+
 public class Value  {
     String value;
     String message;
     List<Registrasi> result;
 
-    public String getValue () {
+    public String getValue ()
+    {
         return value;
     }
 
-    public String getMessage() {
+    public String getMessage()
+    {
         return message;
     }
 
-    public List<Registrasi> getResult() {
+    public List<Registrasi> getResult()
+    {
         return result;
     }
 }
